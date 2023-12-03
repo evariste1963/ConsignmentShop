49018b740346306032227c9c7b797f88a590eaa0
evariste1963 <evariste@hotmail.co.uk>
2023-12-03 16:59:39 +0000

    Add project files.

---
+++ ConsignmentShopUi/ConsignmentShop.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace ConsignmentShopUi
+{
+    partial class ConsignmentShop
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.headertext = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // headertext
+            // 
+            this.headertext.AutoSize = true;
+            this.headertext.Font = new System.Drawing.Font("Ink Free", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.headertext.Location = new System.Drawing.Point(12, 24);
+            this.headertext.Name = "headertext";
+            this.headertext.Size = new System.Drawing.Size(367, 39);
+            this.headertext.TabIndex = 0;
+            this.headertext.Text = "Consignment Shop Demo";
+            // 
+            // ConsignmentShop
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1200, 692);
+            this.Controls.Add(this.headertext);
+            this.Font = new System.Drawing.Font("Ink Free", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "ConsignmentShop";
+            this.Text = "Consignment Shop Demo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label headertext;
+    }
+}
+
